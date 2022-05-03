1c96c323b314e738399153c13cee19be7bc30242
Thrishasivasakthi <104294406+Thrishasivasakthi@users.noreply.github.com>
2022-05-03 19:55:17 +0530

    Add files via upload

---
+++ nested do while.c
@@ -0,0 +1,16 @@
+ #include<stdio.h>
+ int main()
+ {
+ 	int i=0,j=0;
+ 	do
+ 	{
+ 		printf("value of i is %d\n",i);
+ 		i++;
+	 }while (i<=3);
+	do
+	{
+		printf("value of j is %d\n ",j);
+		j++;
+	}while (j<=3);
+	return 0;
+ }
